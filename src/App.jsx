ba2a678dbdeabc6519ce4800721d47dbb6a5b7a3
TANMAYAGARWALLSKT <tanmaylsktagarwal.1025@gmail.com>
2025-04-04 14:45:08 +0530

    ftcdx

--- src/App.jsx
+++ src/App.jsx
@@ -54,7 +54,7 @@ function AppContent() {
   }, []);
 
   return (
-    <div className="flex h-screen w-full bg-gray-100">
+    <div className="flex h-screen w-full bg-gray-900">
       {user && <Navbar />}
       <main className={`flex-1 transition-all duration-300 ${user && (isMobile ? 'pt-16' : isNavbarOpen ? 'ml-64' : 'ml-20')}`}>
         <div className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8 py-6">
